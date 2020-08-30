27dab4d1f9aafe126fece13d50c4a0e950fb2c64
reese_s <icipherfly@gmail.com>
2020-08-29 21:45:48 -0700

    Added functions so 2 canditates vote tallies can display totalVotes value.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// FACTORY FUNCTION TO ANNOUNCE EACH CANDIDATE //
 var presHopeful = function(fullName){ 
 
     var candidate = {};
@@ -6,13 +7,60 @@ var presHopeful = function(fullName){
     candidate.totalVotes = 0; 
  
     candidate.announceHopeful = function(){
-        console.log(this.name + ", showing election results of " + this.electionResults + " with " + this.totalVotes + " total votes.");
+        console.log(this.name + ", showing election results of " + this.electionResults + " with " + this.totalVotes + 
+        " total votes.");
         };
  
     candidate.announceHopeful();
- 
     return candidate;
 };
- 
+// HERE ARE THE CANDIDATES //
 var candidate1 = presHopeful("Carol Moseley Braun");
 var candidate2 = presHopeful("Shirley Chisholm");
+
+// ELECTION RESULTS FOR CAROL //
+candidate1.electionResults = [5, 1, 7 ,2, 33, 6, 4, 2, 1, 14, 8, 3, 1, 11, 11, 0 ,5 ,3, 3, 3, 7, 4, 8, 9, 3, 7, 
+2 , 2 , 4, 2, 8, 3, 15, 15, 2, 12, 0, 4, 13, 1, 3, 2, 8, 21, 3, 2, 11, 1, 3, 7, 2];
+
+// ELECTION RESULTS FOR SHIRLEY //
+candidate2.electionResults = [4, 2, 4, 4, 22, 3, 3, 1, 2, 15, 8, 1, 3, 9, 0, 6, 1, 5, 5, 1, 3, 7, 8, 1, 3, 3, 1, 
+    3, 2, 2, 6, 2, 14, 0, 1, 6, 7, 3, 7, 3, 6, 1, 3, 17, 3, 1, 2, 11, 2, 3, 1];
+
+// ELECTION UPDATES FOR CAROL //
+candidate1.electionResults[9] = 1;
+candidate1.electionResults[4] = 17;
+candidate1.electionResults[43] = 11;
+
+// ELECTION UPDATES FOR SHIRLEY //
+candidate2.electionResults[9] = 28;
+candidate2.electionResults[4] = 38;
+candidate2.electionResults[43] = 27;
+
+console.log (candidate1.electionResults);
+console.log (candidate2.electionResults);
+
+// FUNCTION FOR CAROL'S TOTAL VOTES //
+candidate1.totalVotes = function(){
+    this.totalVotes = 0;
+
+    for (var i = 0; i < this.electionResults.length; i++){
+    
+        this.totalVotes = this.totalVotes + this.electionResults[i];
+    } 
+};  
+
+candidate1.totalVotes();
+console.log(candidate1.totalVotes);
+
+
+// FUNCTION FOR SHIRLEY'S TOTAL VOTES //
+candidate2.totalVotes = function(){
+    this.totalVotes = 0;
+
+    for (var i = 0; i < this.electionResults.length; i++){
+    
+        this.totalVotes = this.totalVotes + this.electionResults[i];
+    } 
+};  
+candidate2.totalVotes();
+console.log(candidate2.totalVotes);
